7fc01bc74cca0232c943edd978f9cf28366ab2a3
Richa Agrawal <riagrawal@teksystems.com>
2022-03-30 17:49:34 +0530

    Added acess specifier file

---
+++ javaLessons/src/javaLessons/acessSpecifiers.java
@@ -0,0 +1,29 @@
+package javaLessons;
+
+public class acessSpecifiers{
+	
+	public static void main(String[] args) {
+		
+		specifier.display();
+//		privateSpecifier obj = new privateSpecifier();
+//		obj.display();
+	}
+	
+}
+	
+	class specifier{
+	
+		public static void display() {
+			System.out.println("this is private specifier");
+		}
+}
+
+//public class acessSpecifiers {
+//	
+//	public static void main(String[] args) {
+//		
+////		privat/eSpecifier.display();
+//	}
+//	
+//
+//}
